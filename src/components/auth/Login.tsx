851d977170da7507a64f376fcd965e81786673ae
Shlok D. Mange <shlok_mange@protonmail.com>
2021-03-25 12:45:50 +0400

    Fixed Input Designs

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -11,25 +11,25 @@ const Login: React.FC = () => {
     
     return(
         <section className="space-y-8">
-            <div className="flex flex-col space-y-2 font-mono">
+            <div className="flex flex-col space-y-2 font-mono text-green-400">
                 <label htmlFor="email">Email</label>
                 <input 
                     type="text" 
-                    className="px-3 py-2 text-lg focus:outline-none" 
+                    className="px-3 py-2 text-lg focus:outline-none bg-gray-900 rounded-md" 
                     placeholder="john.doe@email.com"
                 />
             </div>
-            <div className="flex flex-col space-y-2 font-mono">
+            <div className="flex flex-col space-y-2 font-mono text-green-400">
                 <label htmlFor="password">Password</label>
                 <input 
                     type="password" 
-                    className="px-3 py-2 text-lg focus:outline-none" 
+                    className="px-3 py-2 text-lg focus:outline-none bg-gray-900 rounded-md" 
                     placeholder="*********"
                 />
             </div>
             <div className="flex justify-center font-mono">
                     <button 
-                        className="p-2 bg-green-500 text-gray-300 w-full"
+                        className="p-2 bg-gray-900 text-green-400 w-full"
                         onClick={gotoDashboard}
                     >
                         LOGIN
