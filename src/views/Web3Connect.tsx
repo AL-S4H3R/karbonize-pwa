dd9c0f8c5e29eca4c23dac1be07c1a46aabfa22b
Shlok D. Mange <shlok_mange@protonmail.com>
2021-03-25 23:23:39 +0400

    Portis Connection Added

--- src/views/Web3Connect.tsx
+++ src/views/Web3Connect.tsx
@@ -1,14 +1,28 @@
+import React from 'react'
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { IonPage } from '@ionic/react'
-import React from 'react'
 import { useHistory } from 'react-router'
-import WalletHeader from '../components/wallet/WalletHeader'
+
+import Web3 from 'web3'
+import Portis from '@portis/web3'
 
 const Web3Connect: React.FC = () => {
 
     const history = useHistory()
 
+    const connectToPortis = async () => {
+        const portis = new Portis('b358dd9f-7a6a-4c26-948b-a79c29d3fa59', 'maticMumbai')
+        const web3 = new Web3(portis.provider)
+
+        var accounts = await web3.eth.getAccounts()
+        console.log(accounts[0])
+
+        if(accounts !== null){
+            history.push('/wallet')
+        }
+    }
+    
     return(
         <IonPage>
             <div>
@@ -23,7 +37,7 @@ const Web3Connect: React.FC = () => {
                 </nav>
                 <section className="flex justify-center font-mono text-green-500 px-10 py-4">
                     <div className="w-full space-y-8">
-                        <button className="bg-gray-900 p-2 rounded-md w-full border border-green-800">Connect to Portis</button>
+                        <button onClick={connectToPortis} className="bg-gray-900 p-2 rounded-md w-full border border-green-800">Connect to Portis</button>
                         <button className="bg-gray-900 p-2 rounded-md w-full">Connect to Arkane</button>
                         <button className="bg-gray-900 p-2 rounded-md w-full">Connect to Coinbase</button>
                     </div>
